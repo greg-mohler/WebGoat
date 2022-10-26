7a01229135cb24225fb50d971f8f7bc0aa0cc7d3
greg-mohler <59029196+greg-mohler@users.noreply.github.com>
2022-10-26 16:16:30 -0400

    Update python_example_1.py

--- python_example_1.py
+++ python_example_1.py
@@ -3,14 +3,13 @@ import logging
 
 logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.DEBUG)
 
+hostname = 'acme.com'
+
 user = 'MyServiceAccount'
 password = 'SecurePass123!@#'
 
 session = requests.Session()
 session.auth = (user, password)
 
-auth = session.post('http://' + hostname)
-response = session.get('http://' + hostname + '/rest/applications')
-
-
-
+auth = session.post('https://' + hostname)
+response = session.get('https://' + hostname + '/rest/applications')
